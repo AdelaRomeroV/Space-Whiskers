c76305dadd242b97cd837af4972bc3bcdfa3e13a
AJRV26 <ajrvjarinee17@gmail.com>
2023-12-06 17:48:12 -0500

    pantalla de pausa

---
+++ Space Whiskers/Assets/HUB/Main Menu/MenuPausa.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuPausa : MonoBehaviour
+{
+    public GameObject botonPausa;
+    public GameObject menuPausa;
+
+    public void Pausa()
+    {
+            Time.timeScale = 0f;
+            botonPausa.SetActive(false);
+            menuPausa.SetActive(true);
+    }
+
+    public void Reanudar()
+    {
+        Time.timeScale = 1f;
+        botonPausa.SetActive(true);
+        menuPausa.SetActive(false);
+    }
+}
