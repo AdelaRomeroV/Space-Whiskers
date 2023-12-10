e962e72a2df0d39b767024214b4203880ed97725
AJRV26 <ajrvjarinee17@gmail.com>
2023-12-10 02:27:49 -0500

    Arreglo del npc, tipografia de los textos, pantalla de start

--- Space Whiskers/Assets/HUB/NPC's/NPCs.cs
+++ Space Whiskers/Assets/HUB/NPC's/NPCs.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 [System.Serializable]
 public struct Dialogue
 {
-    public Sprite sprite;
+    public Image imagen;
     [TextArea(4, 6)] public string line;
 }
 public class NPCs : MonoBehaviour
